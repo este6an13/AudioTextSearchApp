0da5d1d3068c1e98d181a2bf65aadec44516082d
este6an13 <57577966+este6an13@users.noreply.github.com>
2021-02-16 12:21:19 -0500

    initial commit with all the files

---
+++ AudioTextSearchApp/Controllers/AudiosController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AudioTextSearchApp.Data;
+using AudioTextSearchApp.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace AudioTextSearchApp.Controllers
+{
+    public class AudiosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AudiosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Audios
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Audio.ToListAsync());
+        }
+
+        // GET: Audios/SearchForm
+        public async Task<IActionResult> SearchForm()
+        {
+            return View();
+        }
+
+        // POST: Audios/SearchResults
+        public async Task<IActionResult> SearchResults(String SearchPhrase)
+        {
+            return View("Index", await _context.Audio.Where( a => a.Text.Contains(SearchPhrase) ).ToListAsync());
+        }
+
+
+
+        // GET: Audios/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var audio = await _context.Audio
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (audio == null)
+            {
+                return NotFound();
+            }
+
+            return View(audio);
+        }
+
+        // GET: Audios/Create
+        [Authorize]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Audios/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Text")] Audio audio)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(audio);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(audio);
+        }
+
+        // GET: Audios/Edit/5
+        [Authorize]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var audio = await _context.Audio.FindAsync(id);
+            if (audio == null)
+            {
+                return NotFound();
+            }
+            return View(audio);
+        }
+
+        // POST: Audios/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Text")] Audio audio)
+        {
+            if (id != audio.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(audio);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AudioExists(audio.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(audio);
+        }
+
+        // GET: Audios/Delete/5
+        [Authorize]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var audio = await _context.Audio
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (audio == null)
+            {
+                return NotFound();
+            }
+
+            return View(audio);
+        }
+
+        // POST: Audios/Delete/5
+        [Authorize]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var audio = await _context.Audio.FindAsync(id);
+            _context.Audio.Remove(audio);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AudioExists(int id)
+        {
+            return _context.Audio.Any(e => e.Id == id);
+        }
+    }
+}
